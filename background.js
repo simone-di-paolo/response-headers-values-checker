18d2afc38366cd9cf255b51e576a37462d8678b9
Simone Di Paolo <s.dipaolo90@gmail.com>
2023-02-13 11:40:37 +0100

    bugfix/small-improvements

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 try {
     // on page change
     chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        if (changeInfo.status === 'complete') {
+        if (!tab.url.startsWith("chrome://") && changeInfo.status === 'complete') {
             chrome.scripting.executeScript({
                 files: ['contentScript.js'],
                 target: {
