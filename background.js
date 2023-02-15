2d44e9be9314421363407b391a116cf3d6923b7f
Simone Di Paolo <s.dipaolo90@gmail.com>
2023-02-16 00:20:42 +0100

    Feature - Major update. Added the start/stop button. Added a countdown timer for refresh after the button click. Improved logic that will make the script work.

--- background.js
+++ background.js
@@ -1,13 +1,9 @@
-try {
-    // on page change
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        if (changeInfo.status === 'complete' && tab.url && !tab.url.startsWith('chrome://extensions/')) {
-            chrome.scripting.executeScript({
-                target: {tabId: tabId},
-                files: ['contentScript.js']
-            });
-        }
-    });
-} catch (e) {
-    console.log(e);
-}
+// on page change
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.url && !tab.url.startsWith('chrome://extensions/')) {
+        chrome.scripting.executeScript({
+            target: {tabId: tabId},
+            files: ['contentScript.js']
+        });
+    }
+});
--- popup/popup.js
+++ popup/popup.js
@@ -1,28 +1,38 @@
 var valuesInput = document.querySelector('#values-input');
 var section = document.getElementById("results-section");
 var div = document.querySelector("#results-div > div");
-var valuesArray = [];
+var startStopButton = document.getElementById("toggle-btn");
+
+var millisBeforeUpdate = 3000;
 
 document.addEventListener('DOMContentLoaded', function(event) {
     init();
+    startStopButton.addEventListener("click", toggleExtension);
+    getIsEnabled();
 });
 
 function init() {
     eventListenerInitializer();
     prepopulateValuesInput();
     setTimeout(function() {
-        var mmm = "";
+        var responseValues = "";
         chrome.storage.local.get('responseValues', function (obj) {
-            mmm = obj.responseValues;
-            console.log("Inside: " + mmm);
-            if(mmm !== null && mmm !== '') {
-                worker();
+            responseValues = obj.responseValues;
+            console.log("Inside: " + responseValues);
+            if(responseValues !== null && responseValues !== '') {
+                chrome.storage.local.get(["enabled"], function (result) {
+                    var isEnabledInFunction = result.enabled || false;
+                    if(isEnabledInFunction) {
+                        worker();
+                    }
+                });
             }
         });
     }, 500);
 }
 
 function worker() {
+    debugger;
     if (valuesInput != null) {
 
         var valuesInputText = valuesInput.value;
@@ -31,32 +41,38 @@ function worker() {
         var headers = "";
 
         chrome.storage.local.get('responseValues', function (obj) {
-            headers = obj.responseValues;
-            if(headers !== '' ) {
+            if(obj.responseValues !== undefined) {
+                headers = obj.responseValues;
+                if (headers !== '') {
 
-                // get headers as an array
-                if (typeof headers !== undefined) {
-                    var headersArray = headers.trim().split(/[\r\n]+/);
-    
-                    var foundValues = [];
-                    var count = 0;
+                    // get headers as an array
+                    if (headers !== undefined) {
+                        var headersArray = headers.trim().split(/[\r\n]+/);
 
-                    for (var i = 0; i < values.length; i++) {
-                        for (var j = 0; j < headersArray.length; j++) {
-                            if (headersArray[j].startsWith(values[i])) {
-                                foundValues[count] = headersArray[j]
-                                count++;
+                        var foundValues = [];
+                        var count = 0;
+
+                        for (var i = 0; i < values.length; i++) {
+                            for (var j = 0; j < headersArray.length; j++) {
+                                if (headersArray[j].startsWith(values[i])) {
+                                    foundValues[count] = headersArray[j]
+                                    count++;
+                                }
+                            }
+                        }
+                        if (foundValues.length > 0) {
+                            injector(foundValues);
+                        } else {
+                            if (!section.classList.contains("disabled")) {
+                                section.classList.add("disabled");
                             }
                         }
                     }
-                    if (foundValues.length > 0) {
-                        injector(foundValues);
-                    } else {
-                        if (!section.classList.contains("disabled")) {
-                            section.classList.add("disabled");
-                        }
-                    }
                 }
+            } else {
+                // removing childs from div
+                var element = document.querySelector(".results-div-style");
+                removeChildsFromNode(element);
             }
         });
     }
@@ -116,5 +132,139 @@ function injector(foundValues) {
         span.className = 'results-span';
         span.innerText = foundValues[l];
         div.appendChild(span);
+    }
+}
+
+function toggleExtension() {
+    var button = document.getElementById("toggle-btn");
+    if(button !== undefined) {
+        chrome.storage.local.get(["enabled"], function (result) {
+            var isEnabledInFunction = result.enabled || false;
+            if (!isEnabledInFunction) {
+                // enabling
+                chrome.storage.local.set({enabled: true}, function () {
+                    //document.getElementById("toggle-btn").textContent = "ENABLED";
+                    buttonStyler(button, "ENABLED", "#4caf50");
+                });
+
+
+                refreshAndReload(millisBeforeUpdate);
+            } else {
+                // disabling
+                // clear the local storage
+                chrome.storage.local.clear();
+
+                // set again the same value in storage
+                chrome.storage.local.set({'valuesInput': valuesInput.value}, function() {
+                    // nothing, just save and that's all!
+                    // •|龴◡龴|•
+                });
+
+                chrome.storage.local.set({enabled: false}, function () {
+                    //document.getElementById("toggle-btn").textContent = "DISABLED";
+                    buttonStyler(button, "DISABLED", "#f44336");
+                });
+                refreshAndReload(millisBeforeUpdate);
+                //countdownToRefresh(millisBeforeUpdate/1000);
+            }
+        });
+    } else {
+        console.log("start/stop button is undefined");
+    }
+}
+
+function getIsEnabled() {
+    chrome.storage.local.get(["enabled"], function (result) {
+        var isEnabledInFunction = result.enabled || false;
+        if (startStopButton !== undefined) {
+            var button = document.getElementById("toggle-btn");
+            if (button !== undefined) {
+                isEnabledInFunction ? buttonStyler(button, "ENABLED", "#4caf50") : buttonStyler(button, "DISABLED", "#f44336");
+                startStopButton.classList.remove("disabled");
+            }
+        } else {
+            console.log("startStopButton is undefined");
+        }
+    });
+}
+
+function refreshAndReload(relaunchWorkerTime) {
+    // refresh the page for update the headers response
+    setTimeout(function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.reload(tabs[0].id);
+        });
+    }, 500);
+
+    debugger;
+    countdownToRefresh(relaunchWorkerTime/1000);
+    // relaunching the worker so the popup can be updated
+    setTimeout(function() {
+        worker();
+    }, relaunchWorkerTime);
+}
+
+function buttonStyler(button, buttonText, buttonColor) {
+    if(button !== undefined) {
+        button.textContent = buttonText;
+        button.style.backgroundColor = buttonColor;
+    }
+}
+
+function countdownToRefresh(startsSeconds, ) {
+    var countdownDiv = document.querySelector('#countdown-section');
+    var countdownText = document.querySelector('#countdown-text-id');
+    if(countdownDiv !== undefined) {
+        if(section !== undefined) {
+            section.classList.add("disabled");
+        }
+
+        // checking if the script it's enabled
+        chrome.storage.local.get(["enabled"], function (result) {
+            var isEnabledInFunction = result.enabled || false;
+
+            // if it's enabled show the countdown and continue...
+            if(isEnabledInFunction) {
+                if (startStopButton !== undefined) {
+                    if (countdownDiv.classList.contains("disabled")) {
+                        countdownText.textContent = "Updating values in " + startsSeconds + " seconds...";
+                        countdownDiv.classList.remove("disabled");
+                    }
+
+                    // updating countdown
+                    if (countdownText !== undefined) {
+                        var counter = setInterval(function () {
+                            startsSeconds = startsSeconds - 1;
+                            if (startsSeconds <= 0) {
+                                clearInterval(counter);
+                                // hiding the countdown div
+                                countdownDiv.classList.add("disabled");
+                            }
+                            if (startsSeconds > 1) {
+                                countdownText.textContent = "Updating values in " + startsSeconds + " seconds...";
+                            } else {
+                                countdownText.textContent = "Updating values in " + startsSeconds + " second...";
+                            }
+                        }, 1000);
+                    }
+                }
+            } else  {
+                // else it's disabled so remove and hide the results
+                var element = document.querySelector(".results-div-style");
+                removeChildsFromNode(element);
+            }
+        });
+    }
+}
+
+function removeChildsFromNode(element) {
+    // removing childs from div
+    if(typeof element !== undefined && element !== undefined) {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild);
+        }
+    }
+    if (!section.classList.contains("disabled")) {
+        section.classList.add("disabled");
     }
 }
